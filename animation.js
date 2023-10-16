b709d4adaf9ff112a5028c451028c9d3919b105f
ashwinthampi <ashwin@thampi.com>
2023-10-16 01:29:41 -0400

    Edits

--- animation.js
+++ animation.js
@@ -1,7 +1,13 @@
+// Create an IntersectionObserver with a callback function.
 const observer = new IntersectionObserver((entries) => {
+    // Iterate through each entry (element) in the entries array.
     entries.forEach((entry) => {
+        // Add the "show" class to the element's class list when it becomes visible.
         entry.target.classList.add('show');
     });
 });
+// Select all elements with the "hidden" class.
 const hiddenElement = document.querySelectorAll('.hidden');
+// Iterate through each element with the "hidden" class.
+// Instruct the observer to watch (observe) each element.
 hiddenElement.forEach((el) => observer.observe(el));
